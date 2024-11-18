90c1e154752391fbd1186d0581b7e70c1f38c25c
NgocAnh76 <ngocanh76.dev@gmail.com>
2024-11-19 02:20:38 +0700

    update

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import MovieSeat from "./Components/MovieSeat";
+import "../src/Sass/main.scss";
+
+function App() {
+  return (
+    <>
+      <MovieSeat />
+    </>
+  );
+}
+
+export default App;
